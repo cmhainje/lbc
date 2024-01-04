e9dccb51ebf0638a1d4ce3ca171d9b00854014a3
Connor Hainje <cmhainje@gmail.com>
2024-01-04 14:30:10 -0500

    update training script to include submission, loading, saving

---
+++ scripts/submit_train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:20:00
+#SBATCH --mem=10GB
+#SBATCH --gres=gpu
+#SBATCH --job-name=lbc_train
+
+module purge
+
+singularity exec --nv \
+    --overlay /home/ch4407/py/overlay-15GB-500K.ext3:rw \
+    /scratch/work/public/singularity/cuda11.8.86-cudnn8.7-devel-ubuntu22.04.2.sif* \
+    /bin/bash -c "source /ext3/env.sh; venv lbc; cd /home/ch4407/lbc/scripts; python train.py 100000"
+
--- scripts/train.py
+++ scripts/train.py
@@ -2,6 +2,8 @@ import numpy as np
 import jax
 import equinox as eqx
 import optax
+import cloudpickle
+import argparse
 
 from lbc.models import AutoEncoder
 
@@ -9,8 +11,13 @@ from glob import glob
 from tqdm.auto import tqdm
 
 
+# Parse number of epochs to train
+ap = argparse.ArgumentParser()
+ap.add_argument('n_epochs', default=10_000, type=int)
+args = ap.parse_args()
+
 # Load images
-filenames = sorted(glob("/Users/connor/data/lbc/58540-processed/*.npy"))
+filenames = sorted(glob("/scratch/ch4407/lbc/58540-processed/*.npy"))
 print(len(filenames), "files found")
 images = np.stack([np.load(fname) for fname in filenames]).reshape(-1, 1, 128, 128)
 images = jax.numpy.array(images)
@@ -46,13 +53,52 @@ def train_step(model, opt_state, x):
     return model, opt_state, loss_value
 
 
-# Try a single train step
-model, opt_state, train_loss = train_step(model, opt_state, images)
-print("Loss after one step:", train_loss)
+history = { "loss": [] }
+
+CHECKPOINT_DIR = '/scratch/ch4407/lbc/checkpoints'
 
 
-# Try a few train steps
-for i in tqdm(range(10_000), desc="Training", unit="step"):
+def save():
+    path = f'{CHECKPOINT_DIR}/checkpoint_{len(history["loss"])}.pkl'
+    print(f'Saving checkpoint at {path}')
+    train_state = ( model, optim, opt_state, history )
+    with open(path, 'wb') as f:
+        cloudpickle.dump(train_state, f)
+
+
+def load(path=None):
+    if path is None:
+        ckpts = glob(f'{CHECKPOINT_DIR}/checkpoint_*.pkl')
+        if len(ckpts) > 0:
+            n_epochs = [ int(c.split('_')[1].split('.')[0]) for c in ckpts ]
+            most_recent = np.argmax(n_epochs)
+            path = ckpts[most_recent]
+
+    if path is None:
+        path = ''
+
+    print(f'Loading checkpoint at {path}')
+    with open(path, 'rb') as f:
+        train_state = cloudpickle.load(f)
+    ( model, optim, opt_state, history ) = train_state
+    return model, optim, opt_state, history
+
+
+try:
+    model, optim, opt_state, history = load()
+except OSError:
+    print("Failed to restore from a recent checkpoint: starting training from scratch.")
+    pass
+
+
+init_loss = reco_loss(model, images)
+print("Initial loss:", init_loss)
+
+# Train
+for i in tqdm(range(args.n_epochs), desc="Training", unit="step"):
     model, opt_state, train_loss = train_step(model, opt_state, images)
+    history['loss'].append(train_loss)
 
-print("Loss after more steps:", train_loss)
+print("Final loss:", history['loss'][-1])
+save()
+
