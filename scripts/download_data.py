8f2d8115fbf06805d6ae5a0e060e414339f7dd7b
Connor Hainje <cmhainje@gmail.com>
2024-01-09 11:06:21 -0500

    update download script to separate arcs and flats and skip the server MOTD

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -11,6 +11,9 @@ import argparse
 import json
 import subprocess
 
+from os import makedirs
+from os.path import join
+
 
 BASE_URL = 'rsync://sdss5@dtn.sdss.org/sdsswork/data/boss/spectro/apo'
 
@@ -35,68 +38,64 @@ def make_filenames(d):
     return fnames
 
 
+def download(file_list, dest):
+    with open(args.tempfile, 'w') as f:
+        if args.test:
+            print('[TESTING MODE] Only going to download the first four files.')
+            f.write('\n'.join(file_list[:4]))
+        else:
+            f.write('\n'.join(file_list))
+
+    rsync_cmd = [
+        "rsync",
+        "-avz",
+        f"--files-from={args.tempfile}",
+        "--no-motd",
+        BASE_URL,
+        dest
+    ]
+    print("Running the following rsync command:")
+    print(f"  {' '.join(rsync_cmd)}")
+
+    process = subprocess.Popen(
+        rsync_cmd,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
+
+    with process.stdout:
+        for line in iter(process.stdout.readline, b''):
+            if not line:
+                break
+            print(line.strip())
+
+    # Wait for the process to complete and get the remaining output
+    stdout, stderr = process.communicate()
+
+    # Print the remaining output
+    print(stdout)
+    print(stderr)
+
+    if process.returncode != 0:
+        print("rsync failed with return code", process.returncode)
+
+
 with open(args.infile, 'r') as f:
     data = json.load(f)
 
-file_list = []
-
 if args.arcs:
     arc_fnames = make_filenames(data['arcs'])
-    print(len(arc_fnames), 'arcs found')
-    file_list.extend(arc_fnames)
+    print(len(arc_fnames), 'arcs to download.')
+    dest = join(args.dest, 'arcs')
+    makedirs(dest, exist_ok=True)
+    download(arc_fnames, dest)
 
 if args.flats:
     flat_fnames = make_filenames(data['flats'])
-    print(len(flat_fnames), 'flats found')
-    file_list.extend(flat_fnames)
-
-print(len(file_list), 'files to download.')
-
-with open(args.tempfile, 'w') as f:
-    if args.test:
-        print('[TESTING MODE] Only going to download the first four files.')
-        f.write('\n'.join(file_list[:4]))
-    else:
-        f.write('\n'.join(file_list))
-
-rsync_cmd = [
-    "rsync",
-    "-avz",
-    f"--files-from={args.tempfile}",
-    BASE_URL,
-    args.dest
-]
-print("Running the following rsync command:")
-print(f"  {' '.join(rsync_cmd)}")
-
-process = subprocess.Popen(
-    rsync_cmd,
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    text=True
-)
-
-with process.stdout:
-    for line in iter(process.stdout.readline, b''):
-        if not line:
-            break
-        print(line.strip())
-
-# Wait for the process to complete and get the remaining output
-stdout, stderr = process.communicate()
-
-# Print the remaining output
-print(stdout)
-print(stderr)
-
-if process.returncode != 0:
-    print("rsync failed with return code", process.returncode)
-
-# print("return code:")
-# print(result.returncode)
-# print("stdout:")
-# print(result.stdout)
-# print("stderr:")
-# print(result.stderr)
+    print(len(flat_fnames), 'flats to download.')
+    dest = join(args.dest, 'flats')
+    makedirs(dest, exist_ok=True)
+    download(flat_fnames, dest)
 
