1f035c498d4c2425785c0422c3405038fcfa5b1a
Connor Hainje <cmhainje@gmail.com>
2024-01-09 16:27:10 -0500

    new training experiments with new data

--- scripts/submit_train.sh
+++ scripts/submit_train.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=00:20:00
-#SBATCH --mem=10GB
-#SBATCH --gres=gpu
-#SBATCH --job-name=lbc_train
+#SBATCH -J lbc_train
+#SBATCH -t 00:30:00
+#SBATCH --mem 10GB
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task 1
+#SBATCH --gres gpu
+#SBATCH --mail-type=all
+#SBATCH --mail-user=ch4407@nyu.edu
+#SBATCH --output=slurm-%x-%j.out
+#SBATCH --error=slurm-%x-%j.out
 
 module purge
 
 singularity exec --nv \
     --overlay /home/ch4407/py/overlay-15GB-500K.ext3:ro \
     /scratch/work/public/singularity/cuda11.8.86-cudnn8.7-devel-ubuntu22.04.2.sif* \
-    /bin/bash -c "source /ext3/env.sh; venv lbc; cd /home/ch4407/lbc/scripts; python train.py 50000 -c 4 -l 128"
+    /bin/bash -c "source /ext3/env.sh; venv lbc; cd /home/ch4407/lbc/scripts; python train.py 50_000 -c 4 -l 8"
 
--- scripts/train.py
+++ scripts/train.py
@@ -6,6 +6,7 @@ import cloudpickle
 import argparse
 
 from lbc.models import AutoEncoder
+from lbc import data as datautils
 
 from glob import glob
 from tqdm.auto import tqdm
@@ -18,22 +19,16 @@ ap.add_argument('n_epochs', default=10_000, type=int)
 ap.add_argument('-c', '--hidden_channels', default=4, type=int)
 ap.add_argument('-l', '--latent_dim', default=256, type=int)
 ap.add_argument('--save_freq', default=100, type=int)
+ap.add_argument('--checkpoint_freq', default=10000, type=int)
 args = ap.parse_args()
 
-# Load images
-filenames = sorted(glob("/scratch/ch4407/lbc/58540-processed/*.npy"))
-print(len(filenames), "files found")
-images = np.stack([np.load(fname) for fname in filenames]).reshape(-1, 1, 128, 128)
-images = jax.numpy.array(images)
-print("Input data shape:", images.shape)
-
-# Create an AutoEncoder
-key = jax.random.PRNGKey(seed=120)
-model = AutoEncoder(hidden_channels=args.hidden_channels, latent_dim=args.latent_dim, key=key)
-
-# Try running the images through the model
-output = jax.vmap(model)(images)
-print("Output data shape:", output.shape)
+# Load data
+# filenames = sorted(glob("/scratch/ch4407/lbc/58540-processed/*.npy"))
+# print(len(filenames), "files found")
+# images = np.stack([np.load(fname) for fname in filenames]).reshape(-1, 1, 128, 128)
+# images = jax.numpy.array(images)
+# print("Input data shape:", images.shape)
+X_train, X_val, X_test = datautils.split(datautils.load())
 
 
 # Make a loss function (just MSE between the images)
@@ -43,7 +38,9 @@ def reco_loss(model, x):
     return mse
 
 
-# Instantiate an optimizer
+# Instantiate model and optimizer
+key = jax.random.PRNGKey(seed=120)
+model = AutoEncoder(hidden_channels=args.hidden_channels, latent_dim=args.latent_dim, key=key)
 eta = 1e-4
 optim = optax.adamw(eta)
 opt_state = optim.init(eqx.filter(model, eqx.is_array))
@@ -57,7 +54,7 @@ def train_step(model, opt_state, x):
     return model, opt_state, loss_value
 
 
-history = { "epoch": [], "loss": [] }
+history = { "epoch": [], "loss": [], "loss_val": [] }
 
 CHECKPOINT_DIR = f'/scratch/ch4407/lbc/checkpoints/h{args.hidden_channels}_l{args.latent_dim:03d}'
 makedirs(CHECKPOINT_DIR, exist_ok=True)
@@ -95,8 +92,8 @@ except OSError:
     pass
 
 
-init_loss = reco_loss(model, images)
-print("Initial loss:", init_loss)
+# init_loss = reco_loss(model, images)
+# print("Initial loss:", init_loss)
 
 epoch_0 = history['epoch'][-1] if len(history['epoch']) > 0 else 0
 
@@ -105,14 +102,27 @@ n_epochs = args.n_epochs
 if (epoch_0 + n_epochs) % args.save_freq == 0:
     n_epochs += 1
 
-for i in tqdm(range(n_epochs), desc="Training", unit="step"):
-    model, opt_state, train_loss = train_step(model, opt_state, images)
+for i, batch in tqdm(
+    zip(range(n_epochs), datautils.dataloader(X_train)),
+    desc="Training", unit="step", total=n_epochs,
+):
+    batch = jax.numpy.array(batch)
+    model, opt_state, train_loss = train_step(model, opt_state, batch)
 
     if i % args.save_freq == 0:
+        # Compute validation loss
+        loss_val = reco_loss(model, X_val)
+
         history['epoch'].append(epoch_0 + i)
         history['loss'].append(train_loss)
+        history['loss_val'].append(loss_val)
 
-print("Final loss:", history['loss'][-1])
+    if i > 0 and i % args.checkpoint_freq == 0:
+        save()
+
+print("Final losses:")
+print("  train:", history['loss'][-1])
+print("    val:", history['loss_val'][-1])
 
 save()
 
