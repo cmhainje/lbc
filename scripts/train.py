a5c43eaa9c723ed11756aff6a156068df7413959
Connor Hainje <cmhainje@gmail.com>
2024-01-25 17:12:14 -0500

    use newer dataloader and fix the epoch numbers in the saved history

--- scripts/train.py
+++ scripts/train.py
@@ -6,7 +6,7 @@ import cloudpickle
 import argparse
 
 from lbc.models import AutoEncoder
-from lbc import data as datautils
+from lbc.data import load as load_data, dataloader
 
 from glob import glob
 from tqdm.auto import tqdm
@@ -18,17 +18,12 @@ ap = argparse.ArgumentParser()
 ap.add_argument('n_epochs', default=10_000, type=int)
 ap.add_argument('-c', '--hidden_channels', default=4, type=int)
 ap.add_argument('-l', '--latent_dim', default=256, type=int)
+ap.add_argument('--camera', default='r', type=str)
 ap.add_argument('--save_freq', default=100, type=int)
 ap.add_argument('--checkpoint_freq', default=10000, type=int)
 args = ap.parse_args()
 
-# Load data
-# filenames = sorted(glob("/scratch/ch4407/lbc/58540-processed/*.npy"))
-# print(len(filenames), "files found")
-# images = np.stack([np.load(fname) for fname in filenames]).reshape(-1, 1, 128, 128)
-# images = jax.numpy.array(images)
-# print("Input data shape:", images.shape)
-X_train, X_val, X_test = datautils.split(datautils.load())
+X_train, X_val, X_test = load_data(camera=args.camera)
 
 
 # Make a loss function (just MSE between the images)
@@ -56,7 +51,7 @@ def train_step(model, opt_state, x):
 
 history = { "epoch": [], "loss": [], "loss_val": [] }
 
-CHECKPOINT_DIR = f'/scratch/ch4407/lbc/checkpoints/h{args.hidden_channels}_l{args.latent_dim:03d}'
+CHECKPOINT_DIR = f'/scratch/ch4407/lbc/checkpoints_{args.camera}/h{args.hidden_channels}_l{args.latent_dim:03d}'
 makedirs(CHECKPOINT_DIR, exist_ok=True)
 
 def save():
@@ -77,8 +72,9 @@ def load(path=None):
 
     if path is None:
         path = ''
+    else:
+        print(f'Loading checkpoint at "{path}"')
 
-    print(f'Loading checkpoint at {path}')
     with open(path, 'rb') as f:
         train_state = cloudpickle.load(f)
     ( model, optim, opt_state, history ) = train_state
@@ -92,32 +88,27 @@ except OSError:
     pass
 
 
-# init_loss = reco_loss(model, images)
-# print("Initial loss:", init_loss)
-
-epoch_0 = history['epoch'][-1] if len(history['epoch']) > 0 else 0
-
 # Train
+epoch_0 = history['epoch'][-1] if len(history['epoch']) > 0 else 0
 n_epochs = args.n_epochs
-if (epoch_0 + n_epochs) % args.save_freq == 0:
-    n_epochs += 1
 
 for i, batch in tqdm(
-    zip(range(n_epochs), datautils.dataloader(X_train)),
+    zip(range(n_epochs), dataloader(X_train)),
     desc="Training", unit="step", total=n_epochs,
 ):
     batch = jax.numpy.array(batch)
     model, opt_state, train_loss = train_step(model, opt_state, batch)
 
-    if i % args.save_freq == 0:
+    step_num = i + 1
+    if step_num % args.save_freq == 0:
         # Compute validation loss
         loss_val = reco_loss(model, X_val)
 
-        history['epoch'].append(epoch_0 + i)
+        history['epoch'].append(epoch_0 + step_num)
         history['loss'].append(train_loss)
         history['loss_val'].append(loss_val)
 
-    if i > 0 and i % args.checkpoint_freq == 0:
+    if step_num % args.checkpoint_freq == 0:
         save()
 
 print("Final losses:")
@@ -127,4 +118,3 @@ print("    val:", history['loss_val'][-1])
 save()
 
 print("Done!")
-
